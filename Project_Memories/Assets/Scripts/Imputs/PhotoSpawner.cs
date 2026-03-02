3ab9f0593f5e4ec70244ea5f13bf34db19a64482
FelipeSousaP <comffelipe@gmail.com>
2026-03-02 19:41:02 -0300

    FEAT: GERADOR DE FOTOS
    Adicionei um gerador de fotos usando o design pattern, object pool

---
+++ Project_Memories/Assets/Scripts/Imputs/PhotoSpawner.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PhotoSpawner : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] GameObject PhotoPrefab;
+    [SerializeField] GameObject PhotoConteiner;
+    [SerializeField] Transform[] PhotoPosition;
+    [SerializeField] int QuantidadeDEFotos;
+
+    private void Start()
+    {
+        for (int i = 0; i < QuantidadeDEFotos; i++) 
+        {
+            GameObject obj = Instantiate(PhotoPrefab,PhotoPosition[i].position,Quaternion.identity,PhotoConteiner.transform);
+            Debug.Log("Criado");
+        }
+    }
+}
