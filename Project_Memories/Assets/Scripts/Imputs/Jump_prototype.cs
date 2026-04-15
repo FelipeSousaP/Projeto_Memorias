5be06f06a1a262a6b5c471ebb0830734c7d3685b
FelipeSousaP <comffelipe@gmail.com>
2026-04-15 11:33:27 -0300

    bug: sistema de movimento e pulo
    devo alterar para fisica.

--- Project_Memories/Assets/Scripts/Imputs/Jump_prototype.cs
+++ Project_Memories/Assets/Scripts/Imputs/Jump_prototype.cs
@@ -1,9 +1,62 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Memorias.Gameplay.Player
 {
     public class PlayerJump: MonoBehaviour
     {
+        #region Etapa 1: Requisitos
+        [Header("Jump Settings")]
+        [SerializeField] private InputActionReference _jumpAction;
+        [SerializeField] private float _jumpForce;
+        [SerializeField] private Transform _player;
+        [SerializeField] private bool _isJumping;
 
+        [Header("RayCast Settings")]
+        [SerializeField] private float _distanceRay;
+        [SerializeField] private LayerMask _layerMask;
+        #endregion
+
+        float _velocity;
+        #region Etapa 2: input por evento
+        private void OnEnable()
+        {
+            if (_jumpAction != null)
+            {
+                _jumpAction.action.performed += JumpPerfomed;
+                _jumpAction.action.canceled += JumpPerfomed;
+            }
+        }
+        private void OnDisable()
+        {
+            _jumpAction.action.performed -= JumpPerfomed;
+            _jumpAction.action.canceled -= JumpPerfomed;
+        }
+        public void JumpPerfomed(InputAction.CallbackContext callbackContext)
+        {
+            _isJumping = callbackContext.ReadValueAsButton();
+            _velocity = _jumpForce;
+        }
+        #endregion
+
+        //caso esteja no sol, ele pode pular
+        private void Update()
+        {
+            if (IsGrounded())
+            {
+                if (_isJumping) 
+                {
+                    _player.transform.position = new Vector3(0,_velocity,0) * Time.deltaTime;
+                    Debug.Log("Ele pulo EBBAAAA");
+                }
+            }
+        }
+
+        private bool IsGrounded()
+        {
+            Vector3 end = Vector3.down * _distanceRay;
+            Debug.DrawLine(_player.transform.position, end,Color.red);
+            return Physics.Raycast(_player.transform.position, -transform.up, _distanceRay, _layerMask);
+        }
     }
 }
--- Project_Memories/Assets/Scripts/Imputs/MovIment_Prototype.cs
+++ Project_Memories/Assets/Scripts/Imputs/MovIment_Prototype.cs
@@ -16,7 +16,7 @@ namespace Memorias.Gameplay.Player
         [SerializeField] private float _distanceRay;
         [SerializeField] private LayerMask _layer;
         
-        private Vector2 _value;
+        private Vector3 _value;
         private Vector3 _finalMoviment;
         #endregion 
         #region Etapa 2: Input do jogador
@@ -33,22 +33,24 @@ namespace Memorias.Gameplay.Player
             _moveAction.action.performed -= MovePerformed;
             _moveAction.action.canceled -= MoveCanceled;
         }
-        public void MovePerformed(InputAction.CallbackContext c) => _value = c.ReadValue<Vector2>();
+        public void MovePerformed(InputAction.CallbackContext c)
+        {
+            _value = c.ReadValue<Vector3>();
+        }
         public void MoveCanceled(InputAction.CallbackContext c) => _value = Vector2.zero;
         #endregion
         #region Etapa 3 Ações
         private void Update()
         {
-            // 1. Define a intenção de movimento
-            _finalMoviment = new Vector3(_value.x, 0, _value.y);
-
+            //_finalMoviment = new Vector3(_value.x, 0, _value.y);
             // condicional: so se move se tiver Input/ alteração no input
             if (_finalMoviment != Vector3.zero)
             {
                 //condicional: _finalmoviment a direção que o jogador de move é gerado o raio
                 if (!Physics.Raycast(_playerTranform.position, _finalMoviment.normalized, _distanceRay, _layer))
                 {
-                    _playerTranform.Translate(_finalMoviment * _speed * Time.deltaTime);
+                    //_playerTranform.Translate(_finalMoviment * _speed * Time.deltaTime);
+                    _playerTranform.position += _value * _speed * Time.deltaTime;
                     Debug.DrawRay(_playerTranform.position, _finalMoviment.normalized * _distanceRay, Color.green);
                 }
                 else
