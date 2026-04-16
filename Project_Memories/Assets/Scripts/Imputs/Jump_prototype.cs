882c76a4bdd6a72d3d50d1cb4cb3bf13c35e98e0
FelipeSousaP <comffelipe@gmail.com>
2026-04-15 21:52:54 -0300

    feat: mais coisas

--- Project_Memories/Assets/Scripts/Imputs/Jump_prototype.cs
+++ Project_Memories/Assets/Scripts/Imputs/Jump_prototype.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.VFX;
 
-public class Jump_prototype : MonoBehaviour
+namespace Memorias.Gameplay.Player
 {
+<<<<<<< HEAD
     [SerializeField] InputActionReference JumpAction;
     [SerializeField] Rigidbody rb;
     [SerializeField] float Speed;
@@ -14,9 +15,31 @@ public class Jump_prototype : MonoBehaviour
     [SerializeField] private VisualEffect _jumpParticles;
 
     private void OnEnable()
+=======
+    public class PlayerJump: MonoBehaviour
+>>>>>>> refactor/player-moviment
     {
-        if (JumpAction != null)
+        #region Etapa 1: Requisitos
+        [Header("Input Settings")]
+        [SerializeField] private InputActionReference _jumpAction;
+        [SerializeField] private int _Quantospulospodedar = 0;
+        [SerializeField] private int _Quantospulosdeu;
+
+        [Header("Physics Settings")]
+        [SerializeField] private float _jumpForce;
+        [SerializeField] private Rigidbody _rb;
+
+        [Header("RayCast Settings")]
+        [SerializeField] private Transform _player;
+        [SerializeField] private float _distanceRay;
+        [SerializeField] private LayerMask _layerMask;
+
+        #endregion
+
+        #region Etapa 2: input por evento
+        private void OnEnable()
         {
+<<<<<<< HEAD
             JumpAction.action.performed += Jump;
             JumpAction.action.canceled += Jump;
         }
@@ -47,3 +70,53 @@ public class Jump_prototype : MonoBehaviour
         }
     }
 }
+=======
+            if (_jumpAction != null)
+            {
+                _jumpAction.action.performed += JumpPerfomed;
+                _jumpAction.action.canceled += JumpPerfomed;
+            }
+        }
+        private void OnDisable()
+        {
+            _jumpAction.action.performed -= JumpPerfomed;
+            _jumpAction.action.canceled -= JumpPerfomed;
+        }
+        public void JumpPerfomed(InputAction.CallbackContext callbackContext)
+        {
+            if (callbackContext.performed && callbackContext.ReadValueAsButton())
+            {
+                if (IsGrounded())
+                {
+                    _Quantospulosdeu = 0;
+                    Debug.Log("No chão: Contador resetado.");
+                }
+
+                if (_Quantospulosdeu < _Quantospulospodedar)
+                {
+                    _rb.linearVelocity = new Vector3(_rb.linearVelocity.x, 0, _rb.linearVelocity.z); //Para resetar a velocidade causada pelos inputs do move + o Pulo
+                    _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+                    _Quantospulosdeu++;
+                    Debug.Log($"Pulou! Total de pulos: {_Quantospulosdeu}");
+                }
+            }
+        }
+        #endregion
+
+        private bool IsGrounded()
+        {
+            return Physics.Raycast(_player.position, Vector3.down, _distanceRay, _layerMask);
+        }
+
+        #if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            if (_player == null) return;
+            Gizmos.color = IsGrounded() ? Color.green : Color.red;
+            Vector3 direction = Vector3.down * _distanceRay;
+            Gizmos.DrawRay(_player.position, direction);
+        }
+        #endif
+    }
+}
+>>>>>>> refactor/player-moviment
