947fe7a06fe593942074e0622b90478e00f96e99
FelipeSousaP <comffelipe@gmail.com>
2026-04-22 23:21:04 -0300

    fix: tentando corrigir movimento da camera

--- Project_Memories/Assets/Collect/Photos.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Photos : MonoBehaviour
-{
-    public string name = "Teste";
-    public CanvasGroup foto;
-    
-    [HideInInspector] public PhotoSpawner chefe;
-}
--- Project_Memories/Assets/UI/Diario_Prototype.cs
+++ Project_Memories/Assets/UI/Diario_Prototype.cs
@@ -9,6 +9,7 @@ public class Diario_Prototype : MonoBehaviour
 
     private void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         DiarioAberto = false;
     }
 
@@ -16,10 +17,10 @@ public class Diario_Prototype : MonoBehaviour
     {
         if (UiAction.action.WasPressedThisFrame())
         {
-            EstadoDoDiario();
+            DiaryStatus();
         }
     }
-    void EstadoDoDiario()
+    void DiaryStatus()
     {
         DiarioAberto = !DiarioAberto;
         if (!DiarioAberto)
@@ -35,9 +36,11 @@ public class Diario_Prototype : MonoBehaviour
     void Esconder(CanvasGroup uiDiario)
     {
         UIManeger.Instance.Hide(uiDiario);
+        Cursor.lockState = CursorLockMode.Locked;
     }
     void Mostrar(CanvasGroup uiDiario)
     {
         UIManeger.Instance.Show(uiDiario);
+        Cursor.lockState = CursorLockMode.None;
     }
 }
