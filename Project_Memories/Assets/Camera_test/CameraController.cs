1e08004a1ae05771c85e376abacc6d15cebf5604
FelipeSousaP <comffelipe@gmail.com>
2026-04-22 23:02:16 -0300

    Feat: Corrigindo o Cinemachine

---
+++ Project_Memories/Assets/Camera_test/CameraController.cs
@@ -0,0 +1,10 @@
+using System.Net.Http.Headers;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
