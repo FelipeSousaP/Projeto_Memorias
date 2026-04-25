f0ebdd6860750ccfef9867b660137a896ec7ecfb
FelipeSousaP <comffelipe@gmail.com>
2026-04-25 12:05:26 -0300

    fix: ObjectPool corrigndo
    O object pool está reutilizando as mesmas fotos e ainda está mandando para pontos diferentes

--- Project_Memories/Assets/Game/Gameplay/Player/scripts/CollectAction.cs
+++ Project_Memories/Assets/Game/Gameplay/Player/scripts/CollectAction.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using Memorias.Framework.ObjectPool;
+using Memorias.System.PhotoManeger;
 namespace Memorias.Gameplay.Player
 {
     public class CollectAction : MonoBehaviour
     {
+        [SerializeField] private PhotoSpawner _photoSpawner;
         private void OnTriggerEnter(Collider other)
         {
             if (other.TryGetComponent<Photos>(out Photos component)) //Substituir por GetComponent<Photo>() 
@@ -11,6 +13,7 @@ namespace Memorias.Gameplay.Player
                 Debug.Log($"Coletado: {component._name}");
                 ObjectPoolController.Instance.objectPool.SetPool(other.gameObject);
                 other.gameObject.SetActive(false);
+                _photoSpawner.PlayerIsTouched();
             }
         }
     }
