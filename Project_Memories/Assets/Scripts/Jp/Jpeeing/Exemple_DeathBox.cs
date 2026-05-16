b3c9c2f0719e93c47a5c53346e987ae57de54aef
FelipeSousaP <comffelipe@gmail.com>
2026-05-16 10:25:33 -0300

    Feat: implementando um sistema

--- Project_Memories/Assets/Scripts/Jp/Jpeeing/Exemple_DeathBox.cs
+++ Project_Memories/Assets/Scripts/Jp/Jpeeing/Exemple_DeathBox.cs
@@ -1,16 +1,18 @@
+using Memorias.System.SpawnManeger;
 using UnityEngine;
 
 public class Exemple_DeathBox : MonoBehaviour {
-    [SerializeField] private Vector3 _inicialPos;
+    //[SerializeField] private Vector3 _inicialPos;
     [SerializeField] private GameObject player;
 
-    private void Start() {
-        _inicialPos = player.transform.position;
-    }
+    //private void Start() {
+    //    _inicialPos = player.transform.position;
+    //}
 
     private void OnTriggerEnter(Collider other) {
         if (other.transform.CompareTag("Player")) {
-            player.transform.position = _inicialPos;
+            Debug.Log(player.transform.position);
+            player.transform.position = Spawnmaneger.Instance.ReturnCheckPoint();
         }
     }
 }
