faa6d6e3da55f77c69cf29f475f8c57c4f7b090f
jprvivasGS <jprvivas@gmail.com>
2026-05-16 18:26:58 -0300

    feat: adicionado parte 1 da parte 2

--- Project_Memories/Assets/Scripts/Jp/Jpeeing/Exemple_Button.cs
+++ Project_Memories/Assets/Scripts/Jp/Jpeeing/Exemple_Button.cs
@@ -1,12 +1,37 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Exemple_Button : MonoBehaviour
 {
     [SerializeField] private UnityEvent _onclick;
+    [SerializeField] private LayerMask _player;
+    [SerializeField] private float Distance;
+    [SerializeField] private GameObject box;
+    [SerializeField] private GameObject[] points;
+    [SerializeField] private Rigidbody rb;
+    public Vector3 firstSpawn;
+    public Vector3 secondSpawn;
+    RaycastHit hit;
 
+    private void Start() {
+        firstSpawn = points[0].transform.position;
+        secondSpawn = points[1].transform.position;
+        box.transform.position = points[0].transform.position;
+        rb = box.GetComponent<Rigidbody>();
+    }
     public void EnableTower()
     {
         _onclick.Invoke();
+    }
+
+    private void Update() {
+        if (Physics.Raycast(transform.position, Vector3.up, out RaycastHit hit, Distance, _player)) {
+            if (box.transform.position.y < this.transform.position.y) {
+                rb.isKinematic = true;
+                box.transform.position = points[0].transform.position;
+                rb.isKinematic = false;
+            }
+        }
     }
 }
--- Project_Memories/Assets/Scripts/Jp/Jpeeing/Exemple_DeathBox.cs
+++ Project_Memories/Assets/Scripts/Jp/Jpeeing/Exemple_DeathBox.cs
@@ -2,12 +2,7 @@ using Memorias.System.SpawnManeger;
 using UnityEngine;
 
 public class Exemple_DeathBox : MonoBehaviour {
-    [SerializeField] private Vector3 _inicialPos;
     [SerializeField] private GameObject player;
-
-   private void Start() {
-        _inicialPos = player.transform.position;
-    }
 
     private void OnTriggerEnter(Collider other) {
         if (other.transform.CompareTag("Player")) {
